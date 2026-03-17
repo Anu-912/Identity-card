d47c4f861d92f4a4e79fcc785833a9b1827476fc
Anu-912 <anudariochiredene@gmail.com>
2026-03-17 11:19:33 +0800

    Update Card.jsx
    img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-export default function ({sgender,sname,semail,studentcode}) {
+export default function ({sgender,sname,semail,studentcode,idpic}) {
   return (
     <div className="flex flex-col items-center">
 <div className="w-[600px] h-[400px] rounded-3xl bg-blue-400 p-6">
@@ -16,7 +16,7 @@ export default function ({sgender,sname,semail,studentcode}) {
 <p className="text-[20px] text-black">{semail}</p>
 </div>
 <div className="flex flex-col">
-  <img className="h-20 w-6xl" src="https://th.bing.com/th/id/R.f83c07d0f3b9b5b675978840fb853a94?rik=yKuXjlBMWPzsRA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-dZ4VlGvH9vc%2fUBlJXsjGreI%2fAAAAAAAARvA%2fSKXP-99TZaM%2fs1600%2ffunny-cat-pictures-009-045.jpg&ehk=grXffVQODzBpt9kVJIK8a8AiYziWaeb6tev7AIOtw4I%3d&risl=&pid=ImgRaw&r=0"/>
+              <img className="h-[250px] w-[250px] object-cover " src={idpic} />
 <p className="text-[20px] text-black" >{studentcode}</p>
 </div>
 </div>
